9de6a97e816eb2f6e411c35785b007ffc87f95fa
Laufreyd <an.lepage@iia-laval.fr>
2017-08-01 00:12:48 +0200

    Automatic sorting
    Automatic sorting by ascending
    Rearrangement of code

--- diagrammCreator.js
+++ diagrammCreator.js
@@ -12,19 +12,9 @@ function circularDiagramm(resultsArray){
   //On vérifie la validité de l'argument de resultsArray
   if(typeof(resultsArray) == "object"){
 
-    //Initialisation des données utilisés à l'avenir
-    var coordonneesCenterDiagramm = [250, 250], rayonDiagramm = 250;
+    //Gère l'initialisation et la finalisation
+    generateCircularDiagramm(resultsArray);
 
-    //Initialisation du conteneur du diagramme
-    var container = document.getElementById('DiagrammeCirculaire');
-
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute('width', 500);
-    svg.setAttribute('height', 500);
-
-    svg = generateSVGBody(resultsArray, svg, coordonneesCenterDiagramm, rayonDiagramm);
-
-    container.appendChild(svg);
   }
   else{
     console.error("Les résultats ne sont pas sous forme d'objet");
@@ -32,16 +22,37 @@ function circularDiagramm(resultsArray){
 }
 
 /**
-* Fonction qui générera le contenu de la balise "svg" qui est le diagramme
+* Fonction qui gère l'initialisation et la finalisation
+*
+* @param resultsArray{ Object }: Contient les résultats
+*/
+function generateCircularDiagramm(resultsArray){
+
+  //Initialisation des données utilisés à l'avenir
+  var coordonneesCenterDiagramm = [250, 250], rayonDiagramm = 250;
+
+  //Initialisation du conteneur du diagramme
+  var container = document.getElementById('DiagrammeCirculaire');
+
+  //Gère l'appel aux différentes fonctions
+  svg = generateSVGBody(resultsArray, coordonneesCenterDiagramm, rayonDiagramm);
+
+  container.appendChild(svg);
+}
+
+/**
+* Fonction qui générera la balise svg et son contenu
 *
 * @param resultsArray{ Object }: Contient les résultats du diagramme
-* @param svg{ Object }: Balise HTML qui contiendra le diagramme
 * @param coordonneesCenterDiagramm{ Object }: Contient les coordonnées du centre du diagramme
 * @param rayonDiagramm{ Number }: Contient le rayon du diagramme
 *
 * @return svg{ Object }: Balise HTML qui contiendra le diagramme
 */
-function generateSVGBody(resultsArray, svg, coordonneesCenterDiagramm, rayonDiagramm){
+function generateSVGBody(resultsArray, coordonneesCenterDiagramm, rayonDiagramm){
+
+  //Création de la balise 'svg'
+  svg = generateSVGTag();
 
   //Tableau qui contient le pourcent du résultat
   resultsInPercent = calculatePercent(resultsArray);
@@ -51,6 +62,23 @@ function generateSVGBody(resultsArray, svg, coordonneesCenterDiagramm, rayonDiag
 
   //Balise svg
   svg = createPaths(svg, coordonneesPoints, coordonneesCenterDiagramm, resultsInPercent, rayonDiagramm);
+
+  return svg;
+}
+
+/**
+* Fonction qui gère la création de la balise svg
+*
+* @return svg{ Object }: Contient la balise svg
+*/
+function generateSVGTag(){
+
+  //Création de la balise svg
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  //Initialisation des attributs
+  svg.setAttribute('width', 500);
+  svg.setAttribute('height', 500);
 
   return svg;
 }
@@ -71,12 +99,48 @@ function calculatePercent(resultsArray){
     totalResults += resultsArray[variableAvancement];
   }
 
-  //Calcule les pourcentages des résultats
+  //Calcule les pourcentages des résultats, arrondi au dixième près
   for(variableAvancement in resultsArray){
-    resultsPercentArray[variableAvancement] = Math.round((resultsArray[variableAvancement] * 100 / totalResults) * 10) / 10;
+
+    //Calcule du pourcentage puis inseretion à l'arrondi 10^-1 près
+    resultInPercent = resultsArray[variableAvancement] * 100 / totalResults;
+    resultsPercentArray[variableAvancement] = Math.round(resultInPercent * 10) / 10;
   }
 
+  //Classe le pourcentages par ordre croissant
+  resultsPercentArray = orderedResults(resultsPercentArray);
+
   return resultsPercentArray;
+}
+
+/**
+* Fonction qui classe les résultats par ordre croissant
+*
+* @param resultsPercentArray{ Object }: Contient les résultats à trier
+*
+* @return orderedResultsPercent{ Object }: Contient les résultats trier par ordre croissant
+*/
+function orderedResults(resultsPercentArray){
+
+  var orderedResultsPercent = [];
+  var valuesResultsPercent = Object.values(resultsPercentArray); //Tableau des valeurs
+  var keysResults = Object.keys(resultsPercentArray); //Tableau des clés
+
+  //Pour chaque élément de l'objet, va trier par ordre croissant
+  for (variableAvancement in resultsPercentArray){
+
+    //On prend le minimum ainsi que son index
+    minimumFound = Math.min(...valuesResultsPercent);
+    indexOfMinimumFound = valuesResultsPercent.indexOf(minimumFound);
+
+    //On l'insère dans le tableau final puis on enlève des autres
+    orderedResultsPercent[keysResults[indexOfMinimumFound]] = minimumFound;
+    valuesResultsPercent.splice(indexOfMinimumFound, 1);
+    keysResults.splice(indexOfMinimumFound ,1);
+
+  }
+  //Renvoie le tableau trié
+  return orderedResultsPercent;
 }
 
 
@@ -98,8 +162,11 @@ function calculateCoordonnesPoints(coordonneesCenterDiagramm, rayonDiagramm, res
 
     //Initialisation du tableau des coordonnées
     coordonneesPoints[variableAvancement] = [];
-    coordonneesPoints[variableAvancement][0] = Math.cos((pourcentAvancement * 3.6) * (Math.PI /180)) * rayonDiagramm + coordonneesCenterDiagramm[0];
-    coordonneesPoints[variableAvancement][1] = Math.abs(Math.sin((pourcentAvancement * 3.6) * (Math.PI /180)) * rayonDiagramm - coordonneesCenterDiagramm[1]);
+
+    degresAvancement = (pourcentAvancement * 3.6) * (Math.PI / 180);
+
+    coordonneesPoints[variableAvancement][0] = Math.cos(degresAvancement) * rayonDiagramm + coordonneesCenterDiagramm[0];
+    coordonneesPoints[variableAvancement][1] = Math.abs(Math.sin(degresAvancement) * rayonDiagramm - coordonneesCenterDiagramm[1]);
 
     pourcentAvancement += resultsInPercent[variableAvancement];
   }
@@ -125,9 +192,9 @@ function createPaths(svg, coordonneesPoints, coordonneesCenterDiagramm, resultsI
   var center = "M" + coordonneesCenterDiagramm[0] + "," + coordonneesCenterDiagramm[1];
   var arc = "A" + coordonneesCenterDiagramm[0] + "," + coordonneesCenterDiagramm[1] + " 0 ";
   var firstPoint, lastPoint, pathString, arcSpecification;
-  var taille = Object.keys(coordonneesPoints).length - 1; //Nombre de résultat
   var keys = Object.keys(resultsInPercent); //Tableau contenant les clés
   var values = Object.values(resultsInPercent); //Tableau contenant les pourcentages
+  var taille = values.length - 1; //Nombre de résultat
 
   //Si il n'ya pas de résultat qui vaut 100%
   if(!values.includes(100)){
@@ -150,7 +217,8 @@ function createPaths(svg, coordonneesPoints, coordonneesCenterDiagramm, resultsI
 
         //Création du dernier point
         if(keys.indexOf(variable) != taille){
-          lastPoint = coordonneesPoints[keys[keys.indexOf(variable)+1]][0] + "," + coordonneesPoints[keys[keys.indexOf(variable)+1]][1];
+          indexNextPoint = keys[keys.indexOf(variable)+1];
+          lastPoint = coordonneesPoints[indexNextPoint][0] + "," + coordonneesPoints[indexNextPoint][1];
         }
         else{
           lastPoint = coordonneesPoints[keys[0]][0] + "," + coordonneesPoints[keys[0]][1];
